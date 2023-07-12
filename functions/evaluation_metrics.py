eb8709c2a366126630a1cb060a8934cb4f59dfd9
Yegor Tchitaev <yegor.tchitaev@gmail.com>
2023-07-12 23:34:34 +1000

    abstract model evaluation and mlflow logging

--- functions/evaluation_metrics.py
+++ functions/evaluation_metrics.py
@@ -11,16 +11,42 @@ class AverageTypes(Enum):
     NONE = auto()
 
 
-def calculate_metric(model, X_data, y_data, metric_name, average: AverageTypes = AverageTypes.MACRO):
-    metric_func_map = {
-        'accuracy': metrics.accuracy_score,
-        'precision': lambda y_true, y_pred: metrics.precision_score(y_true, y_pred, average=average.value),
-        'recall': lambda y_true, y_pred: metrics.recall_score(y_true, y_pred, average=average.value),
-        'f1': lambda y_true, y_pred: metrics.f1_score(y_true, y_pred, average=average.value),
-        'rmse': lambda y_true, y_pred: sqrt(mean_squared_error(y_true, y_pred))
-    }
-    metric_func = metric_func_map.get(metric_name)
-    if metric_func is None:
-        raise ValueError(f"Unsupported metric: {metric_name}")
-    metric = metric_func(y_data, model.predict(X_data))
-    return metric
+class MetricCalculator:
+    def __init__(self, model):
+        self.model = model
+
+    def calculate_metric(self, X, y, metric, additional=None):
+        metric_func_map = {
+            'accuracy': metrics.accuracy_score,
+            'precision': lambda y_true, y_pred: metrics.precision_score(y_true, y_pred, **additional),
+            'recall': lambda y_true, y_pred: metrics.recall_score(y_true, y_pred, **additional),
+            'f1': lambda y_true, y_pred: metrics.f1_score(y_true, y_pred, **additional),
+            'rmse': lambda y_true, y_pred: sqrt(mean_squared_error(y_true, y_pred))
+        }
+        metric_func = metric_func_map.get(metric)
+        if metric_func is None:
+            raise ValueError(f"Unsupported metric: {metric}")
+        metric_value = metric_func(y, self.model.predict(X))
+        return metric_value
+
+class MetricFactory:
+    def __init__(self, model):
+        self.model = model
+
+    def create_metrics(self, config, X_validation, X_test, y_validation, y_test):
+        metrics_dict = {}
+
+        calculator = MetricCalculator(self.model)
+
+        for metric, metric_config in config.items():
+            additional = metric_config.get('additional', {})
+
+            metric_name_validation = f"validation_{metric}"
+            metric_value_validation = calculator.calculate_metric(X_validation, y_validation, metric, additional)
+            metrics_dict[metric_name_validation] = metric_value_validation
+
+            metric_name_test = f"test_{metric}"
+            metric_value_test = calculator.calculate_metric(X_test, y_test, metric, additional)
+            metrics_dict[metric_name_test] = metric_value_test
+
+        return metrics_dict
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import traceback
 
 import mlflow
 import mlflow.lightgbm
+
 import lightgbm as lgb
 
 from functions.config_mapping import GLOBAL_MAPPING, EXPERIMENT_MAPPING
@@ -14,8 +15,7 @@ from functions.data_loader import load_data
 from functions.data_splitter import split_dataset
 from functions.model_runner import create_model
 from functions.evaluation_runner import perform_grid_search
-from functions.evaluation_metrics import AverageTypes, calculate_metric
-from functions.evaluation_logger import log_evaluation_metrics
+from functions.evaluation_metrics import MetricFactory
 
 def main(config_file):
 
@@ -26,13 +26,12 @@ def main(config_file):
     with mlflow.start_run() as run:
 
         try:
-            # Instantiate logger and run
+            
+            # Instantiate logger, run identifiers and log config as artifact
             run_id, experiment_id, experiment_run_path, model_path = setup_run(run)
             logger, file_handler_path = instantiate_python_logger(experiment_run_path, global_configs['run_temp_subdir'], global_configs['py_log_file_name'])
-            logger.info(f"Started run: {experiment_run_path}")
-
-            # Log the config as artifact
             mlflow_log_artifact_dict_to_json(experiment_id, run_id, global_configs['run_temp_subdir'], "config.json", configs[''])
+            logger.info(f"Started run: {experiment_run_path}")
 
             # Load data
             logger.info("Loading data...")
@@ -46,52 +45,23 @@ def main(config_file):
             logger.info("Loading initial model...")
             model = create_model(configs['model.library_name'], configs['model.model_name'], params={})
 
-            # Run grid search to get best model values and grid search CV run logs
+            # Run grid search to get best model parameters and log artifacts
             logger.info("Running grid search...")
             best_params, cv_results = perform_grid_search(model, X_train, y_train,  **configs['tuning.grid_search'])
-
-            # Train the model with the best parameters
-            logger.info("Training model with best parameters from grid search...")
-            best_model = create_model(configs['model.library_name'], configs['model.model_name'], best_params)
-            best_model.fit(X_train, y_train, callbacks=[lgb.log_evaluation(period=100, show_stdv=True)])
-
-            # Evaluate model
-            logger.info("Evaluating model...")
-
-            validation_accuracy = calculate_metric(best_model, X_validation, y_validation, 'accuracy')
-            test_accuracy = calculate_metric(best_model, X_test, y_test, 'accuracy')
-
-            validation_precision = calculate_metric(best_model, X_validation, y_validation, 'precision', average = AverageTypes.MACRO)
-            test_precision = calculate_metric(best_model, X_test, y_test, 'precision', average = AverageTypes.MACRO)
-
-            validation_recall = calculate_metric(best_model, X_validation, y_validation, 'recall', average = AverageTypes.MACRO)
-            test_recall = calculate_metric(best_model, X_test, y_test, 'recall', average = AverageTypes.MACRO)
-
-            validation_f1 = calculate_metric(best_model, X_validation, y_validation, 'f1', average = AverageTypes.MACRO)
-            test_f1 = calculate_metric(best_model, X_test, y_test, 'f1', average = AverageTypes.MACRO)
-
-            validation_rmse = calculate_metric(best_model, X_validation, y_validation, 'rmse')
-            test_rmse = calculate_metric(best_model, X_test, y_test, 'rmse')
-
-            # Log model, best parameters and artifacts
-            logger.info("Logging model, parameters and artifacts...")
-            mlflow.lightgbm.log_model(best_model, "model")
             mlflow.log_params(best_params)
             mlflow_log_artifact_dict_to_csv(experiment_id, run_id, global_configs['run_temp_subdir'], configs['artefacts.cv_results.file_name'], cv_results)
 
-            # Log evaluations
-            logger.info("Logging evaluations...")
-            #log_evaluation_metrics(configs)
-            mlflow.log_metric("validation_accuracy", validation_accuracy)
-            mlflow.log_metric("test_accuracy", test_accuracy)
-            mlflow.log_metric("validation_precision", validation_precision)
-            mlflow.log_metric("test_precision", test_precision)
-            mlflow.log_metric("validation_recall", validation_recall)
-            mlflow.log_metric("test_recall", test_recall)
-            mlflow.log_metric("validation_f1", validation_f1)
-            mlflow.log_metric("test_f1", test_f1)
-            mlflow.log_metric("validation_rmse", validation_rmse)
-            mlflow.log_metric("test_rmse", test_rmse)
+            # Train the model with the best parameters and log model
+            logger.info("Training model with best parameters from grid search...")
+            best_model = create_model(configs['model.library_name'], configs['model.model_name'], best_params)
+            best_model.fit(X_train, y_train, callbacks=[lgb.log_evaluation(period=100, show_stdv=True)])
+            mlflow.lightgbm.log_model(best_model, "model")
+
+            # Evaluate model
+            logger.info("Evaluating model...")
+            factory = MetricFactory(best_model)
+            metrics = factory.create_metrics(configs['evaluate'], X_validation, X_test, y_validation, y_test)
+            [mlflow.log_metric(metric_name, metric_value) for metric_name, metric_value in metrics.items()]
 
         except Exception as e:
             logger.error(f"Exception occurred during model training: {str(e)}")
