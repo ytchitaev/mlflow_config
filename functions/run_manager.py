05f89716cd6ad1808c638314f05a8523a75345cf
Yegor Tchitaev <yegor.tchitaev@gmail.com>
2023-07-15 23:50:34 +1000

    Change \ to / for linux

--- functions/run_manager.py
+++ functions/run_manager.py
@@ -6,7 +6,7 @@ MLRUNS_DIR = "mlruns"
 OUTPUTS_DIR = "outputs"
 CONFIGS_DIR = "configs"
 ARTIFACTS_DIR = "artifacts"
-MODEL_SUBDIR = "artifacts\model\model.pkl"
+MODEL_SUBDIR = "artifacts/model/model.pkl"
 LAST_EXEC_FILE_NAME = "last_exec.json"
 
 
