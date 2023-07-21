36454b0e60a892aaf9a757275f40838b89f4b599
Yegor Tchitaev <yegor.tchitaev@gmail.com>
2023-07-21 18:12:33 +1000

    formatting

--- loggers/python_logger.py
+++ loggers/python_logger.py
@@ -5,9 +5,12 @@ from enum import Enum
 
 from utils.config_loader import get_config
 
+
 class LoggingTypes(Enum):
     MLFLOW = 'mlflow'
     EXTENSION = 'extension'
+
+
 class StreamToLogger(object):
     """Fake file-like stream object that redirects writes to a logger instance."""
 
@@ -34,17 +37,21 @@ def init_python_logger(cfg: dict, component: LoggingTypes):
     # Create a console handler for logging output to the CLI/IDE
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
 
     # Create a file handler for logging output to a file
-    temp_dir = os.path.join(get_config(cfg, 'execution.experiment_run_path'), get_config(cfg, 'global.temp_dir'))
+    temp_dir = os.path.join(get_config(
+        cfg, 'execution.experiment_run_path'), get_config(cfg, 'global.temp_dir'))
     os.makedirs(temp_dir, exist_ok=True)
     if component == LoggingTypes.MLFLOW:
-        file_handler_path = os.path.join(temp_dir, get_config(cfg, 'global.python_mlflow_log_file_name'))
+        file_handler_path = os.path.join(temp_dir, get_config(
+            cfg, 'global.python_mlflow_log_file_name'))
     elif component == LoggingTypes.EXTENSION:
-        file_handler_path = os.path.join(temp_dir, get_config(cfg, 'global.python_extension_log_file_name'))
+        file_handler_path = os.path.join(temp_dir, get_config(
+            cfg, 'global.python_extension_log_file_name'))
     file_handler = logging.FileHandler(file_handler_path)
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(formatter)
